520e841d6956c46d7de54269d0c819d32a405550
Wasim Madha <you@example.com>
2023-12-02 12:17:22 +0530

    8 models of same flavour

--- run/inference.py
+++ run/inference.py
@@ -172,34 +172,38 @@ def main(cfg: InferenceConfig):
         model2 = load_model(cfg, '/kaggle/input/models-pth-files/lstm_6Feat_8hours_kfold2.pth')
         model3 = load_model(cfg, '/kaggle/input/models-pth-files/lstm_6Feat_8hours_kfold3.pth')
         model4 = load_model(cfg, '/kaggle/input/models-pth-files/lstm_6Feat_8hours_kfold4.pth')
+        model5 = load_model(cfg, '/kaggle/input/models-pth-files/lstm_6Feat_8hours_score_kfold1.pth')
+        model6 = load_model(cfg, '/kaggle/input/models-pth-files/lstm_6Feat_8hours_score_kfold2.pth')
+        model7 = load_model(cfg, '/kaggle/input/models-pth-files/lstm_6Feat_8hours_score_kfold3.pth')
+        model8 = load_model(cfg, '/kaggle/input/models-pth-files/lstm_6Feat_8hours_score_kfold4.pth')
 
     with trace("load test dataloader"):
         test_dataloader1 = get_test_dataloader(cfg)
 
-    with trace("load model2"):
-        ## Feature Extractor 
-        cfg.feature_extractor = OmegaConf.load(r'/kaggle/input/updated-dss-code/kaggle-child-mind-institute-detect-sleep-states/run/conf/feature_extractor/CNNSpectrogram.yaml')
-        ## Decoder 
-        cfg.decoder = OmegaConf.load(r'/kaggle/input/updated-dss-code/kaggle-child-mind-institute-detect-sleep-states/run/conf/decoder/TransformerCNNDecoder.yaml')
+    # with trace("load model2"):
+    #     ## Feature Extractor 
+    #     cfg.feature_extractor = OmegaConf.load(r'/kaggle/input/updated-dss-code/kaggle-child-mind-institute-detect-sleep-states/run/conf/feature_extractor/CNNSpectrogram.yaml')
+    #     ## Decoder 
+    #     cfg.decoder = OmegaConf.load(r'/kaggle/input/updated-dss-code/kaggle-child-mind-institute-detect-sleep-states/run/conf/decoder/TransformerCNNDecoder.yaml')
         
-        cfg.features = ['anglez', 'enmo', 'hour_sin', 'hour_cos']
-        model5 = load_model(cfg, '/kaggle/input/models-pth-files/transformerCNN_kfold1.pth')
-        model6 = load_model(cfg, '/kaggle/input/models-pth-files/transformerCNN_kfold2.pth')
-        model7 = load_model(cfg, '/kaggle/input/models-pth-files/transformerCNN_kfold1.pth')
-        model8 = load_model(cfg, '/kaggle/input/models-pth-files/transformerCNN_kfold1.pth')
+    #     cfg.features = ['anglez', 'enmo', 'hour_sin', 'hour_cos']
+    #     model5 = load_model(cfg, '/kaggle/input/models-pth-files/transformerCNN_kfold1.pth')
+    #     model6 = load_model(cfg, '/kaggle/input/models-pth-files/transformerCNN_kfold2.pth')
+    #     model7 = load_model(cfg, '/kaggle/input/models-pth-files/transformerCNN_kfold1.pth')
+    #     model8 = load_model(cfg, '/kaggle/input/models-pth-files/transformerCNN_kfold1.pth')
 
-    with trace("load test dataloader"):
-        test_dataloader2 = get_test_dataloader(cfg)
+    # with trace("load test dataloader"):
+    #     test_dataloader2 = get_test_dataloader(cfg)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
-    models1 = [model1, model2, model3, model4]
-    models2 = [model5, model6, model7, model8]
+    models1 = [model1, model2, model3, model4, model5, model6, model7, model8]
+    # models2 = [model5, model6, model7, model8]
 
-    keys, preds1 = inference(cfg.duration, test_dataloader1, models1, device, use_amp=cfg.use_amp)
-    keys, preds2 = inference(cfg.duration, test_dataloader2, models2, device, use_amp=cfg.use_amp)
+    keys, preds = inference(cfg.duration, test_dataloader1, models1, device, use_amp=cfg.use_amp)
+    # keys, preds2 = inference(cfg.duration, test_dataloader2, models2, device, use_amp=cfg.use_amp)
 
-    preds = (preds1 * 0.75) + (preds2 * 0.25)
+    # preds = (preds1 * 0.75) + (preds2 * 0.25)
     
     with trace("make submission"):
         sub_df = make_submission(
